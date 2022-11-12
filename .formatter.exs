957417a345c34572b8682830a6899d7621759957
chukinas <jonathan.chukinas@revzilla.com>
2022-11-12 10:25:58 -0500

    [remove] stream_data dependency
    It was still hanging around from before I broke MPTree out into its own
    library

--- .formatter.exs
+++ .formatter.exs
@@ -9,7 +9,7 @@ locals_without_parens = [
 ]
 
 [
-  import_deps: [:typed_struct, :stream_data],
+  import_deps: [:typed_struct],
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
   locals_without_parens: locals_without_parens,
   export: [locals_without_parens: locals_without_parens]
